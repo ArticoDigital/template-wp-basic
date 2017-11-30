561272ee80864653245c0b86b8ede832fc9acd96
juan2ramos <juan2ramos@gmail.com>
2017-11-29 23:51:38 -0500

    template init

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php get_header(); ?>
+    <div class="container">
+        <nav class="nav">
+            <div class="nav-left">
+                <a class="active">Link 1</a>
+                <a>Link 2</a>
+            </div>
+            <div class="nav-center">
+                <a class="brand">
+                    qweqwe
+                </a>
+            </div>
+            <div class="nav-right">
+                <a>Link 3</a>
+                <a>Link 4</a>
+            </div>
+        </nav>
+    </div>
+    <div class="container">
+
+        <div class="row">
+            <div class="col-2">
+                <div class="card is-center">.col-2</div>
+            </div>
+            <div class="col-2">
+                <div class="card is-center">.col-2</div>
+            </div>
+            <div class="col">
+                <div class="card is-center">auto</div>
+            </div>
+            <div class="col-3">
+                <div class="card is-center">.col-3</div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-10">
+                <div class="card is-center">.col-10</div>
+            </div>
+            <div class="col">
+                <div class="card is-center">auto</div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="card is-center">auto</div>
+            </div>
+            <div class="col">
+                <div class="card is-center">auto</div>
+            </div>
+            <div class="is-full-width"></div>
+            <div class="col">
+                <div class="card is-center">auto</div>
+            </div>
+            <div class="col">
+                <div class="card is-center">auto</div>
+            </div>
+        </div>
+        <a class="button">Default</a>
+        <a class="button primary">Primary</a>
+
+    </div>
+<?php get_footer();
