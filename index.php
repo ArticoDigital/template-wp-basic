049209144752994fada990ea16c1229058e00030
juan2ramos <juan2ramos@gmail.com>
2017-11-30 09:21:57 -0500

    ajustes template

--- index.php
+++ index.php
@@ -1,24 +1,10 @@
 <?php get_header(); ?>
     <div class="container">
-        <nav class="nav">
-            <div class="nav-left">
-                <a class="active">Link 1</a>
-                <a>Link 2</a>
-            </div>
-            <div class="nav-center">
-                <a class="brand">
-                    qweqwe
-                </a>
-            </div>
-            <div class="nav-right">
-                <a>Link 3</a>
-                <a>Link 4</a>
-            </div>
-        </nav>
+
     </div>
     <div class="container">
-
-        <div class="row">
+        <h1 class="margin-top-auto-56">Template Wordpress Ártico Digital</h1>
+        <div class="row ">
             <div class="col-2">
                 <div class="card is-center">.col-2</div>
             </div>
@@ -48,16 +34,15 @@
             <div class="col">
                 <div class="card is-center">auto</div>
             </div>
-            <div class="is-full-width"></div>
             <div class="col">
                 <div class="card is-center">auto</div>
             </div>
-            <div class="col">
-                <div class="card is-center">auto</div>
+            <div class="col-2">
+                <div class="card is-center">col-2</div>
             </div>
+            <div class=" card is-full-width">dd</div>
         </div>
-        <a class="button">Default</a>
-        <a class="button primary">Primary</a>
+
 
     </div>
 <?php get_footer();
